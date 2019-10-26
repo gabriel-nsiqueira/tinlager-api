e7ae5978a545805793cd11669144e86a0c872d45
Gabriel <gnevessiqueira@gmail.com>
2019-10-26 16:30:29 -0300

    Init

---
+++ app/Controllers/Http/ProfileController.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const Profile = use('App/Models/Profile');
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with profiles
+ */
+class ProfileController {
+	/**
+   * Show a list of all profiles.
+   * GET profiles
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+	async index() {
+		const profiles = await Profile.query().with('user').fetch();
+		return profiles;
+	}
+
+	/**
+   * Create/save a new profile.
+   * POST profiles
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+	async store({ request, auth }) {
+		const data = request.only([ 'title', 'image' ]);
+		const profile = Profile.create({ user_id: auth.user.id, ...data });
+		return profile;
+	}
+
+	/**
+   * Display a single profile.
+   * GET profiles/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+	async show({ params }) {
+		const profile = await Profile.findOrFail(params.id);
+		return profile;
+	}
+
+	/**
+   * Update profile details.
+   * PUT or PATCH profiles/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+	async update({ params, request, response }) {}
+
+	/**
+   * Delete a profile with id.
+   * DELETE profiles/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+	async destroy({ params, auth, response }) {
+		const profile = await Profile.findOrFail(params.id);
+
+		if (profile.user_id != auth.user.id) {
+			return response.status(401);
+		}
+
+		await profile.delete();
+	}
+}
+
+module.exports = ProfileController;
---
+++ database/migrations/1570301797456_profile_schema.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class ProfileSchema extends Schema {
+	up() {
+		this.create('profiles', (table) => {
+			table.increments();
+			table
+				.integer('user_id')
+				.unsigned()
+				.notNullable()
+				.references('id')
+				.inTable('users')
+				.onUpdate('CASCADE')
+				.onDelete('CASCADE');
+			table.string('title', 240).notNullable();
+			table.string('image', 240);
+			table.timestamps();
+		});
+	}
+
+	down() {
+		this.drop('profiles');
+	}
+}
+
+module.exports = ProfileSchema;
